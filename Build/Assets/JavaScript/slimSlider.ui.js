a532e8751a3eeb7b3f89f0588ff10c748f16b7f1
akaufhold <info@stackfactory.de>
2022-08-02 19:57:02 +0200

    [CHANGE] name of controls class to ui

--- Build/Assets/JavaScript/slimSlider.ui.js
+++ Build/Assets/JavaScript/slimSlider.ui.js
@@ -1,6 +1,6 @@
 import SliderHelpers from './slimSlider.helpers';
 
-export default class SliderControls {
+export default class SliderUI {
 	#sliderContainer;
 	#sliderWrapper;
 	#sliderElements;
@@ -48,40 +48,40 @@ export default class SliderControls {
 	init() {
 		this.controlContainer = document.createElement('div');
 		this.controlContainer.classList.add(this.#controlCssClasses.container.name);
-		this.#opts.controls.dots && this.#addControlDots();
-		this.#opts.controls.arrows && this.#addControlArrows();
+		this.#opts.controls.dots && this.#addUIDots();
+		this.#opts.controls.arrows && this.#addUIArrows();
 		this.#sliderContainer.appendChild(this.controlContainer);
 		this.#opts.controls.dots && this.setActiveDot(this.#opts.controls.dotsCount==='fitRows'?[0]:[...Array(this.#opts.slidesPerRow).keys()]); 
 	}
 
-	#addControlDots() {
+	#addUIDots() {
 		let elementsArray = this.#sliderElements;
 		this.dotContainer = document.createElement('div');
 		this.dotContainer.classList.add(this.#controlCssClasses.container.dotContainer.name);
-		(this.#opts.controls.dotsCount === 'fitRows') && (elementsArray = this.#addControlDotsArray(this.#sliderElements.slice()));
+		(this.#opts.controls.dotsCount === 'fitRows') && (elementsArray = this.#addUIDotsArray(this.#sliderElements.slice()));
 		elementsArray.forEach((_,index) => {
 			this.dotContainer.insertAdjacentHTML('beforeEnd',`<div class="${this.#controlCssClasses.container.dotContainer.dot.name}" data-slide="${index}"></div>`);
 		});
 		this.controlContainer.appendChild(this.dotContainer);
 	}
 
-	#addControlDotsArray(controlArray) {
+	#addUIDotsArray(controlArray) {
 		return controlArray.filter((_,index) => {
 			let controlIndex = (index+1) * this.#opts.slidesPerRow;
 			return this.#sliderElements[controlIndex-1]!==undefined;
 		})
 	}
 
-	#addControlArrows() {
+	#addUIArrows() {
 		this.arrowContainer = {}
 		this.arrowContainer.wrapper = document.createElement('div');
 		this.arrowContainer.wrapper.classList.add(this.#controlCssClasses.container.arrowContainer.name);
-		this.arrowContainer.sliderButtonLeft = this.#addControlArrowsSingle('left');
-		this.arrowContainer.sliderButtonRight = this.#addControlArrowsSingle('right');
+		this.arrowContainer.sliderButtonLeft = this.#addUIArrowsSingle('left');
+		this.arrowContainer.sliderButtonRight = this.#addUIArrowsSingle('right');
 		this.controlContainer.appendChild(this.arrowContainer.wrapper);
 	}
 
-	#addControlArrowsSingle(direction){
+	#addUIArrowsSingle(direction){
 		let arrow = document.createElement('div');
 		arrow.classList.add(`${this.#controlCssClasses.container.arrowContainer.arrow.name}-${direction}`);
 		this.arrowContainer.wrapper.appendChild(arrow);
