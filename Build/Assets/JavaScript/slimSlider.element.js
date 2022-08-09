639dcb5312715bed47d526a52f524fb6c36c0ee2
akaufhold <info@stackfactory.de>
2022-08-09 16:22:34 +0200

    [ADD] additionals for slider elements for clone, rect and cirle transition

--- Build/Assets/JavaScript/slimSlider.element.js
+++ Build/Assets/JavaScript/slimSlider.element.js
@@ -5,9 +5,10 @@ export default class SliderElement {
 	elementWrapper = false;
 	elementIsWrapped = false;
 	elementnode;
-	#opts;
+	opts;
 	#index;
 	#slidesCount;
+	#dataAttrCount;
 
 	constructor(
 		options,
@@ -17,12 +18,14 @@ export default class SliderElement {
 		slidesCount
 	){
 		for (const option of Object.entries(options)) {
-			this.#opts = options;
+			this.opts = options;
 		}
+
 		this.#sliderContainer = sliderContainer;
 		this.elementnode 			= element;
-		this.#index 					= index;
-		this.#slidesCount     = slidesCount;
+		this.#index 					= this.opts.index = index;
+		this.#slidesCount     = this.opts.slidesCount = slidesCount;
+		this.#dataAttrCount   = Object.keys(element.dataset).length;
 		this.init();
 	}
 
@@ -31,61 +34,47 @@ export default class SliderElement {
 		this.isElementWrapped() && this.#setElementWrapper();
 		this.#setElementStyles(this.elementWrapper?this.elementWrapper:this.elementnode);
 		this.#setElementClasses();
-		SliderHelpers.setElClass(this.elementnode,this.#opts.elementClass);
-		this.elementWrapper.appendChild(this.#createElementContentWrapper());
-		this.#createAdditionalElements(this.#opts.transition);
+		SliderHelpers.setElClass(this.elementnode,this.opts.elementClass);
+		this.#dataAttrCount && this.elementWrapper.appendChild(this.#createElementContentWrapper());
+		this.#createExtraElements(this.opts.transition);
 	}
 
 	#setElementWrapperType(type) {
-		this.#opts.elementType = type;
+		this.opts.elementType = type;
 	}
 
 	isElementWrapped() {
-		return this.elementIsWrapped = (this.#opts.vignette || this.#opts.zoomOnHover || typeof this.elementnode.dataset==='object') && true;
+		return this.elementIsWrapped = (this.opts.vignette || this.opts.zoomOnHover || typeof this.elementnode.dataset==='object') && true;
 	}
 
 	#setElementWrapper() {
 		this.#index===0 && (this.#sliderContainer.innerHTML = '');
 		this.elementWrapper = SliderHelpers.wrapAround(
 			this.elementnode,
-			SliderHelpers.createWrapperElement(this.#opts.elementWrapperClass,this.#opts.elementType)
+			SliderHelpers.createWrapperElement(this.opts.elementWrapperClass,this.opts.elementType)
 		);
 		this.#sliderContainer.insertAdjacentElement('beforeEnd',this.elementWrapper);
-		this.#sliderContainer.classList.add(`slider-color-${this.#opts.colorTheme}`);
+		this.#sliderContainer.classList.add(`slider-color-${this.opts.colorTheme}`);
 	}
 
 	#setElementStyles(el) {
-		let curColumn = this.#opts.slidesRowWrap ? (this.#index+1)%this.#opts.slidesPerRow : (this.#index+1);
+		let curColumn = this.opts.slidesRowWrap ? (this.#index+1)%this.opts.slidesPerRow : (this.#index+1);
 		SliderHelpers.setElStyle(el,'gridRowStart',1);
-		(curColumn === 0) ? SliderHelpers.setElStyle(el,'gridColumnStart',this.#opts.slidesPerRow) : SliderHelpers.setElStyle(el,'gridColumnStart',curColumn);
+		(curColumn === 0) ? SliderHelpers.setElStyle(el,'gridColumnStart',this.opts.slidesPerRow) : SliderHelpers.setElStyle(el,'gridColumnStart',curColumn);
 	}
 
 	#setElementClasses() {
-		this.#opts.type === 'gallery' && SliderHelpers.setElClass(this.elementWrapper,'parallel');
-		this.#opts.zoomOnHover && SliderHelpers.setElClass(this.elementWrapper,'zoom');
-		this.#opts.vignette && SliderHelpers.setElClass(this.elementWrapper,'vignette');
-	}
-
-	#createAdditionalElements(transition) {
-		transition==='circle' && this.#createAddtionalElementsCirle();
-		transition==='rect' && this.#createAddtionalElementsRect();
-		((transition==='circle') || (transition==='rect')) && this.#sliderContainer.style.setProperty('--stroke-width', `${100/this.#opts.svgAmount*2}%`);
-	}
-
-	#createAddtionalElementsCirle(){
-		this.elementWrapper.insertAdjacentElement('beforeend',SliderHelpers.createSvg('circle','center',this.#opts.svgAmount));
-	}
-
-	#createAddtionalElementsRect(){
-		this.elementWrapper.insertAdjacentElement('beforeend',SliderHelpers.createSvg('rect','center',this.#opts.svgAmount));
+		this.opts.type === 'gallery' && SliderHelpers.setElClass(this.elementWrapper,'parallel');
+		this.opts.zoomOnHover && SliderHelpers.setElClass(this.elementWrapper,'zoom');
+		this.opts.vignette && SliderHelpers.setElClass(this.elementWrapper,'vignette');
 	}
 
 	#createElementContentWrapper() {
 		let {head,text,link} = this.elementnode.dataset;
 		let headerWrap, textWrap, linkWrap, elementWrapper = SliderHelpers.createWrapperElement('slider-image-overlay','div');
-		elementWrapper.classList.add(`overlay-style-${this.#opts.elementOverlayStyle}`);
-		elementWrapper.style.fontSize = `${11-this.#opts.slidesPerRow}px`;
-		head && (headerWrap = this.#createElementContent(head,'header',this.#opts.headerTag)) && (elementWrapper = SliderHelpers.wrapAround(headerWrap,elementWrapper));
+		elementWrapper.classList.add(`overlay-style-${this.opts.elementOverlayStyle}`);
+		elementWrapper.style.fontSize = `${11-this.opts.slidesPerRow}px`;
+		head && (headerWrap = this.#createElementContent(head,'header',this.opts.headerTag)) && (elementWrapper = SliderHelpers.wrapAround(headerWrap,elementWrapper));
 		text && (textWrap = this.#createElementContent(text,'description','p')) && (elementWrapper = SliderHelpers.wrapAround(textWrap,elementWrapper));
 		link && (linkWrap = this.#createElementContent('mehr','link','a')) && (elementWrapper =  SliderHelpers.wrapAround(linkWrap,elementWrapper));
 		return elementWrapper;
@@ -97,5 +86,25 @@ export default class SliderElement {
 			textNode,
 			SliderHelpers.createWrapperElement(`slider-${cssClass}`,htmlTag)
 		);
+	}
+
+	#createExtraElements(transition) {
+		transition==='circle' && this.#createEECirle();
+		transition==='rect' && this.#createEERect();
+		transition==='clones' && this.#createEEClones();
+		((transition==='circle') || (transition==='rect')) && this.#sliderContainer.style.setProperty('--stroke-width', `${100/this.opts.transitionSegments*2}%`);
+	}
+
+	#createEECirle(){
+		this.elementWrapper.insertAdjacentElement('beforeend',SliderHelpers.createSvg('circle','center',this.opts.transitionSegments));
+	}
+
+	#createEERect(){
+		this.elementWrapper.insertAdjacentElement('beforeend',SliderHelpers.createSvg('rect','center',this.opts.transitionSegments));
+	}
+
+	async #createEEClones(){
+		let clonedElements = await SliderHelpers.createClones(this.elementnode,'rect',this.opts);
+		this.elementWrapper.insertAdjacentElement('beforeend',clonedElements);
 	}
 }
--- Build/Assets/JavaScript/slimSlider.helpers.js
+++ Build/Assets/JavaScript/slimSlider.helpers.js
@@ -29,7 +29,6 @@ export default class SliderHelpers {
 	}
 
 	static wrapAround(el, wrapper) {
-		//console.log(el, el.nodeType); 
 		(el.length > 1 && el.nodeType !== 3) ? el.forEach(el => wrapper.appendChild(el)) : wrapper.appendChild(el);
 		return wrapper;
 	}
@@ -82,29 +81,70 @@ export default class SliderHelpers {
 	}
 
 	static createSvg(svgType, align='left', sectionsTotal) {
-		console.log(svgType, align, sectionsTotal);
 		let circle = document.createElement('div');
 		circle.classList.add('slider-transition-overlay',align);
 		let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 		svg.id = `${svgType}Svg`;
 		svg.classList.add(`${svgType}-amount-${sectionsTotal}`);
-		svgType === 'circle' && (svg = this.createSvgCircle(svg,sectionsTotal));
-		svgType === 'rect' && (svg = this.createSvgRect(svg,sectionsTotal));
+		svgType === 'circle' && (svg = this.createSvgCircles(svg,sectionsTotal));
+		svgType === 'rect' && (svg = this.createSvgRects(svg,sectionsTotal));
 		circle.appendChild(svg);
 		return circle;
 	}
 
-	static createSvgCircle(svg, sectionsTotal) {
+	static createSvgCircles(svg, sectionsTotal) {
 		for (var i = 0; i < sectionsTotal; i++) {
 			svg.innerHTML += `<circle id="circle${i}" class="circle${i}" cx="50%" cy="50%" r="${(100/sectionsTotal*i)}%"></circle>`;
 		}
 		return svg;
 	}
 
-	static createSvgRect(svg, sectionsTotal) {
+	static createSvgRects(svg, sectionsTotal) {
 		for (var i = 0; i < sectionsTotal; i++) {
 			svg.innerHTML += `<rect id="rect${i}" class="rect${i}" x="${100/sectionsTotal*i}%" y="0" width="${100/sectionsTotal}%" height="100%"></circle>`;
 		}
 		return svg;
+	}
+
+	static async createClones(originalImg, cloneType, options) {
+		let cloneWrapper = document.createElement('div');
+		cloneWrapper.classList.add('slider-transition-overlay');
+		cloneType === 'rect' && (cloneWrapper = this.#createCloneRects(originalImg,cloneWrapper,options));
+		return cloneWrapper;
+	}
+
+	static async #createCloneRects(originalImg, cloneWrapper, options) {
+		let translateY = `${100*(Math.floor(options.index/options.slidesPerRow))}%`;
+		let loadedImg = await this.#getLoadedElement(originalImg);
+		for (var i = 0; i < options.transitionSegments; i++) {
+			let clone = document.createElement('div');
+			this.setElClass(clone,`slider-transition-clone`);
+			this.setElStyle(clone,'transform',`translateY(${translateY})`);
+			this.setElStyle(clone,'width',`${100/options.transitionSegments}%`);
+			clone.appendChild(this.#createCloneRectsImages(originalImg, loadedImg, i, options));
+			cloneWrapper.appendChild(clone);
+		}
+		return cloneWrapper;
+	}
+
+	static #createCloneRectsImages(originalImg, loadedImg, i, options){
+		let clonedImg = document.createElement('img');
+		clonedImg.style.transitionDelay = clonedImg.style.animationDelay = `${i/1000*(options.transitionDuration/options.transitionSegments)}s`;
+		clonedImg.style.transitionDuration = `${0.3+i/1000*(options.transitionDuration/options.transitionSegments)}s`;
+		clonedImg.src = originalImg.src;
+		clonedImg.width = loadedImg.target.clientWidth;
+		clonedImg.height = loadedImg.target.clientHeight;
+		clonedImg.style.left = `${i*-100}%`;
+		clonedImg.id = `slider-transition-clone-img${i}`;
+		this.setElClass(clonedImg,`slider-transition-clone-img`);
+		return clonedImg;
+	}
+
+	static async #getLoadedElement(element) {
+		return new Promise(function (resolve, reject) {
+			element.addEventListener('load',resolve);
+			element.addEventListener('error', () => reject(new Error(`Failed to load img > ${image.src}`)), false);
+			element.dispatchEvent(new Event('load'));
+		});
 	}
 }
