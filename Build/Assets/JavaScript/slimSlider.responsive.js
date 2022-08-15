715a8e11d0b425f8e8d9bc0527c434e42ae27413
akaufhold <info@stackfactory.de>
2022-08-15 16:17:13 +0200

    [ADD] responsive slider objects and reinit by viewport change

--- Build/Assets/JavaScript/slimSlider.responsive.js
+++ Build/Assets/JavaScript/slimSlider.responsive.js
@@ -1,52 +1,56 @@
 import Slider from './slimSlider';
 
 export default class SliderResponsive {
-	sliders = [];
+	#sliders = [];
+	#sliderCssClass;
+	#sliderSelector;
 	viewportWidth;
-	breakpoints;
+	#breakpoints;
 	#opts;
 
 	constructor(
-		options
+		options,
+		sliderCssClass = `.${options.sliderClass}`
 	){
 		for (const option of Object.entries(options)) {
 			this.#opts = options;
 		}
+		this.#sliderCssClass = sliderCssClass;
+		this.#sliderSelector = document.querySelectorAll(sliderCssClass);
 		this.init();
 	}
 
 	init() {
-		this.setViewport();
-		this.getBreakpoints();
-		this.initSlider();
+		this.#setViewport();
+		this.#getBreakpoints();
+		this.#initSlider();
 		window.addEventListener('resize',this.reInitSlider.bind(this), true);
 	}
 
-	setViewport() {
+	#setViewport() {
 		this.viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 	}
 
-	getBreakpoints() {
+	#getBreakpoints() {
 		const {responsive} = this.#opts;
-		this.breakpoints = responsive.map(el => el.breakpoint);
+		this.#breakpoints = responsive.map(el => el.breakpoint);
 	}
 
 
-	setViewportOptions() {
-		let viewportOptions = this.getOverrideOptionsForViewport();
-		let overrideOptions = viewportOptions[0].options;
-		console.log(this.#opts);
-		for (const option of Object.entries(overrideOptions)) {
-			this.#opts[option[0]] = option[1];
+	#setViewportOptions() {
+		let viewportOptions = this.#getOverrideOptionsForViewport();
+		let overrideOptions = viewportOptions[0]?.options;
+		if (overrideOptions) {
+			for (const option of Object.entries(overrideOptions)) {
+				this.#opts[option[0]] = option[1];
+			}
 		}
-		console.log(this.#opts);
-		//this.viewportWidth
 	}
 
-	getOverrideOptionsForViewport() {
+	#getOverrideOptionsForViewport() {
 		let viewportOptions;
 		let minViewport=0;
-		let target = this.breakpoints.filter(el => el > this.viewportWidth);
+		let target = this.#breakpoints.filter(el => el > this.viewportWidth);
 		if (target.length){
 			minViewport = target.reduce((last,next) => Math.min(last, next));
 		}
@@ -56,18 +60,17 @@ export default class SliderResponsive {
 		return viewportOptions;
 	}
 
-	initSlider() {
-		document.querySelectorAll(`.${this.#opts.sliderClass}`).forEach(sliderElement => {
-			console.log(sliderElement);
-			//sliderElement.innerHTML = '';
-			this.sliders = [];
-			this.sliders.push(new Slider(sliderElement,this.#opts));
+	#initSlider() {
+		this.#sliderSelector.forEach(sliderElement => {
+			this.#sliders.push(new Slider(sliderElement,this.#opts));
 		});
 	}
 
 	reInitSlider() {
-		this.setViewport();
-		this.setViewportOptions();
-		this.initSlider();
+		this.#setViewport();
+		this.#setViewportOptions();
+		this.#sliders.forEach(slider => {
+			slider.init();
+		})
 	}
 }
